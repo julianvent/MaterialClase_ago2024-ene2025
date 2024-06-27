48ac830c200e4010c66072648086deb280fa97cb
Mundus <zS22017046@estudiantes.uv.mx>
2024-06-27 12:23:57 -0600

    es_par_impar modificado

--- src/main/cpp/Ejercicios/es_par_impar.cpp
+++ src/main/cpp/Ejercicios/es_par_impar.cpp
@@ -4,14 +4,13 @@ int main()
 {
     int number;
 
-    std::cout << "Es par o impar? Ingresa un numero mayor que 0:\n";
+    std::cout << "Es par o impar? Ingresa un numero:\n";
     std::cin >> number;
 
-    if (number < 1)
-        std::cout << "Error. El numero debe ser mayor que 0.";
-    else if (number % 2 == 0)
+    if (number % 2 == 0)
         std::cout << number << " es par.";
     else
         std::cout << number << " es impar."; 
-        
+    
+    return 0;
 }
